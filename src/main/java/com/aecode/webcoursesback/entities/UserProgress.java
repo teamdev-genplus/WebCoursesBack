95abac4dd9d272cec7612dcd220f723eddd1cc4d
ManuelAecode <teamdev.genplus@gmail.com>
2024-10-15 11:42:12 -0500

    Actualizando status de usuario

---
+++ src/main/java/com/aecode/webcoursesback/entities/UserProgress.java
@@ -0,0 +1,65 @@
+package com.aecode.webcoursesback.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_progress")
+public class UserProgress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int progressId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserProfile userProfile;
+
+    @ManyToOne
+    @JoinColumn(name = "session_id", nullable = false)
+    private Class classes;
+
+    @Column(nullable = false)
+    private boolean isCompleted;
+
+    public UserProgress() {
+    }
+
+    public UserProgress(int progressId, UserProfile userProfile, Class classes, boolean isCompleted) {
+        this.progressId = progressId;
+        this.userProfile = userProfile;
+        this.classes = classes;
+        this.isCompleted = isCompleted;
+    }
+
+    public int getProgressId() {
+        return progressId;
+    }
+
+    public void setProgressId(int progressId) {
+        this.progressId = progressId;
+    }
+
+    public UserProfile getUserProfile() {
+        return userProfile;
+    }
+
+    public void setUserProfile(UserProfile userProfile) {
+        this.userProfile = userProfile;
+    }
+
+    public Class getClasses() {
+        return classes;
+    }
+
+    public void setClasses(Class classes) {
+        this.classes = classes;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+}
