8fc150f9661d1a7c87a0729ed718760efcad5e33
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-01-21 10:15:18 -0500

    update gmail config in properties

--- src/main/java/com/aecode/webcoursesback/dtos/PayPalOrderDTO.java
+++ src/main/java/com/aecode/webcoursesback/dtos/PayPalOrderDTO.java
@@ -3,16 +3,17 @@ package com.aecode.webcoursesback.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class PayPalOrderDTO {
-    private double price;
+    private Double total;
     private String currency;
     private String method;
     private String intent;
     private String description;
+    private String cancelUrl;
+    private String successUrl;
+
 }
--- src/main/java/com/aecode/webcoursesback/services/PayPalService.java
+++ src/main/java/com/aecode/webcoursesback/services/PayPalService.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.aecode.webcoursesback.dtos.PayPalOrderDTO;
 import com.paypal.api.payments.Amount;
 import com.paypal.api.payments.Payer;
 import com.paypal.api.payments.Payment;
@@ -16,51 +18,81 @@ import com.paypal.api.payments.Transaction;
 import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.PayPalRESTException;
 
+@Service
 public class PayPalService {
 
     @Autowired
     private APIContext apiContext;
 
-    public Payment createPayment(
-            Double total,
-            String currency,
-            String method,
-            String intent,
-            String description,
-            String cancelUrl,
-            String successUrl) throws PayPalRESTException {
+    public Payment createPayment(PayPalOrderDTO orderDTO) throws PayPalRESTException {
+        // Depurando la información recibida
+        System.out.println("Received orderDTO: " + orderDTO);
+
         Amount amount = new Amount();
-        amount.setCurrency(currency);
-        total = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP).doubleValue();
-        amount.setTotal(String.format("%.2f", total));
+        amount.setCurrency(orderDTO.getCurrency());
+        amount.setTotal(String.format("%.2f", orderDTO.getTotal()));
+
+        // Depurando el monto
+        System.out.println("Amount set to: " + amount.getTotal() + " " + amount.getCurrency());
 
         Transaction transaction = new Transaction();
-        transaction.setDescription(description);
+        transaction.setDescription(orderDTO.getDescription());
         transaction.setAmount(amount);
 
         List<Transaction> transactions = new ArrayList<>();
         transactions.add(transaction);
 
         Payer payer = new Payer();
-        payer.setPaymentMethod(method.toString());
+        payer.setPaymentMethod(orderDTO.getMethod());
+
+        // Depurando el método de pago
+        System.out.println("Payment method set to: " + payer.getPaymentMethod());
 
         Payment payment = new Payment();
-        payment.setIntent(intent.toString());
+        payment.setIntent(orderDTO.getIntent());
         payment.setPayer(payer);
         payment.setTransactions(transactions);
+
         RedirectUrls redirectUrls = new RedirectUrls();
-        redirectUrls.setCancelUrl(cancelUrl);
-        redirectUrls.setReturnUrl(successUrl);
+        redirectUrls.setCancelUrl(orderDTO.getCancelUrl());
+        redirectUrls.setReturnUrl(orderDTO.getSuccessUrl());
+
+        // Depurando las URLs de redirección
+        System.out.println("Cancel URL 1: " + redirectUrls.getCancelUrl());
+        System.out.println("Success URL 1: " + redirectUrls.getReturnUrl());
+
         payment.setRedirectUrls(redirectUrls);
 
-        return payment.create(apiContext);
+        // Depurando el pago
+        System.out.println("Payment created 1: " + payment.toJSON());
+
+        System.out.println("API context: " + apiContext);
+
+        // Realizando la solicitud a PayPal
+        Payment createdPayment = payment.create(apiContext);
+
+        // Depurando el resultado del pago
+        System.out.println("Payment created 2: " + createdPayment.toJSON());
+
+        return createdPayment;
     }
 
     public Payment executePayment(String paymentId, String payerId) throws PayPalRESTException {
+        // Depurando los parámetros recibidos
+        System.out.println("Executing payment with paymentId: " + paymentId + " and payerId: " + payerId);
+
         Payment payment = new Payment();
         payment.setId(paymentId);
-        PaymentExecution paymentExecute = new PaymentExecution();
-        paymentExecute.setPayerId(payerId);
-        return payment.execute(apiContext, paymentExecute);
+
+        PaymentExecution paymentExecution = new PaymentExecution();
+        paymentExecution.setPayerId(payerId);
+
+        // Realizando la ejecución del pago
+        Payment executedPayment = payment.execute(apiContext, paymentExecution);
+
+        // Depurando el resultado de la ejecución del pago
+        System.out.println("Payment executed: " + executedPayment.toJSON());
+
+        return executedPayment;
     }
 }
