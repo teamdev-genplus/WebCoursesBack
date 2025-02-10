bea5fa8e4bd89c79c6e2baf7782ebde8435a787d
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-02-10 15:30:33 -0500

    update study plan order number and sec course wssp link

--- src/main/java/com/aecode/webcoursesback/dtos/StudyPlanDTO.java
+++ src/main/java/com/aecode/webcoursesback/dtos/StudyPlanDTO.java
@@ -2,51 +2,20 @@ package com.aecode.webcoursesback.dtos;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudyPlanDTO {
 
     private int studyplanId;
     private int seccourseId;
     private String unit;
     private int hours;
+    private int orderNumber;
     private List<String> sessions;
 
-    public int getStudyplanId() {
-        return studyplanId;
-    }
-
-    public void setStudyplanId(int studyplanId) {
-        this.studyplanId = studyplanId;
-    }
-
-    public int getSeccourseId() {
-        return seccourseId;
-    }
-
-    public void setSeccourseId(int seccourseId) {
-        this.seccourseId = seccourseId;
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
-    public List<String> getSessions() {
-        return sessions;
-    }
-
-    public void setSessions(List<String> sessions) {
-        this.sessions = sessions;
-    }
 }
--- src/main/java/com/aecode/webcoursesback/entities/StudyPlan.java
+++ src/main/java/com/aecode/webcoursesback/entities/StudyPlan.java
@@ -1,10 +1,16 @@
 package com.aecode.webcoursesback.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "studyplans")
 @SequenceGenerator(name = "studyplan_seq", sequenceName = "studyplan_sequence", allocationSize = 1)
@@ -24,60 +30,12 @@ public class StudyPlan {
     @Column(length = 255)
     private int hours;
 
+    @Column(nullable = true, length = 255)
+    private int orderNumber;
+
     @ElementCollection
     @CollectionTable(name = "studyplan_session", joinColumns = @JoinColumn(name = "studyplan_id"))
     @Column(name = "session")
     private List<String> sessions = new ArrayList<>();
 
-
-    public StudyPlan() {
-    }
-
-    public StudyPlan(int studyplanId, String unit, SecondaryCourses secondary_course, int hours, List<String> sessions) {
-        this.studyplanId = studyplanId;
-        this.unit = unit;
-        this.secondary_course = secondary_course;
-        this.hours = hours;
-        this.sessions = sessions;
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
-
-    public SecondaryCourses getSecondary_course() {
-        return secondary_course;
-    }
-
-    public void setSecondary_course(SecondaryCourses secondary_course) {
-        this.secondary_course = secondary_course;
-    }
-
-    public int getStudyplanId() {
-        return studyplanId;
-    }
-
-    public void setStudyplanId(int studyplanId) {
-        this.studyplanId = studyplanId;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
-    public List<String> getSessions() {
-        return sessions;
-    }
-
-    public void setSessions(List<String> sessions) {
-        this.sessions = sessions;
-    }
 }
