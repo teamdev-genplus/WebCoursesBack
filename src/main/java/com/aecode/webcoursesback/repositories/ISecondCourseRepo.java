354493421f465f4f1886dede195f5b5142acf889
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-02-13 17:26:47 -0500

    second course paginator list

--- src/main/java/com/aecode/webcoursesback/repositories/ISecondCourseRepo.java
+++ src/main/java/com/aecode/webcoursesback/repositories/ISecondCourseRepo.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ISecondCourseRepo extends JpaRepository<SecondaryCourses, Integer> {
 
     @Query("SELECT s FROM SecondaryCourses  s WHERE s.programTitle = :programTitle AND s.module = :module")
     SecondaryCourses findByModulexProgram(@Param("module") String module, @Param("programTitle") String programTitle);
+
+    @Query(value = "SELECT * FROM secondary_courses WHERE seccourse_id > :offsetCourseId  ORDER BY seccourse_id ASC  LIMIT :limit;", nativeQuery = true)
+    List<SecondaryCourses> paginatedList(@Param("limit") int limit, @Param("offsetCourseId") int offsetCourseId);
 }
