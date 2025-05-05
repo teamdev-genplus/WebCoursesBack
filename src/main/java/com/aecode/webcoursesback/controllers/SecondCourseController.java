a10501ceaf099148fb6aa4cad14de41e01a58de1
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-05-05 00:32:06 -0500

    update

--- src/main/java/com/aecode/webcoursesback/controllers/SecondCourseController.java
+++ src/main/java/com/aecode/webcoursesback/controllers/SecondCourseController.java
@@ -18,14 +18,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
