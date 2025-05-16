a4d85b974111c4e9f92f89786af2a803b1603c8e
ManuelAecode <teamdev.genplus@gmail.com>
2025-05-16 12:53:45 -0500

    cambiando properties a update-corrigiendo

--- src/main/java/com/aecode/webcoursesback/controllers/UserCourseController.java
+++ src/main/java/com/aecode/webcoursesback/controllers/UserCourseController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+/*
 @RestController
 @RequestMapping("/usercourse")
 public class UserCourseController {
@@ -83,3 +83,4 @@ public class UserCourseController {
         ucS.insert(uca);
     }
 }
+*/
